5ba21fc0f83ccfce7b39ddb5267796dad1f12783
Ian Torres <iantorres@outlook.com>
2025-03-21 00:21:51 -0300

    Between added.

---
+++ sources/include/strong/between.hpp
@@ -0,0 +1,27 @@
+#pragma once
+//          Copyright Ian Torres 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef STRONG_BETWEEN_HPP
+#define STRONG_BETWEEN_HPP
+
+#include <optional>
+#include <string_view>
+
+namespace strong {
+/**
+ * Between
+ *
+ * @param content
+ * @param begin
+ * @param end
+ * @return std::optional<std::string_view>
+ */
+std::optional<std::string_view> between(std::string_view content,
+                                        std::string_view begin,
+                                        std::string_view end);
+}  // namespace strong
+
+#endif
---
+++ tests/between.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+//          Copyright Ian Torres 2025.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#include <strong/between.hpp>
+
+TEST(Strong, Between) {
+  ASSERT_EQ(strong::between("abcdef", "ab", "ef").value(), "cd");
+  ASSERT_EQ(strong::between("ab0000000000de", "ab", "de").value(),
+            "0000000000");
+  ASSERT_EQ(strong::between("010", "0", "0").value(), "1");
+  ASSERT_EQ(strong::between("01110", "0", "0").value(), "111");
+  ASSERT_EQ(strong::between("0111011111111", "0", "0").value(), "111");
+  ASSERT_EQ(strong::between("01110111111110", "0", "0").value(), "111");
+}
